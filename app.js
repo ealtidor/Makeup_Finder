a0ea1de48b8f76cbd32f1ad49f73bff5c84bcc20
ealtidor <emanuella.altidor@gmail.com>
2021-04-19 13:28:52 -0400

    Data Rendering Complete

--- app.js
+++ app.js
@@ -2,9 +2,19 @@
 
 // Create dynamic drop down menu from first request
 
+const getOptions = async() => {
+  const url = 'http://makeup-api.herokuapp.com/api/v1/products.json'
 
+  try {
+    const response = await axios.get(url)
+    console.log(response.data)
+}
+  catch (error) {
+    console.error(error)
+  }
+}
 
-
+getOptions()
 
 
 
