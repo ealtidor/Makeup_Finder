8b47857783de3d0f7728cb9f2af8fba1ae096a1e
ealtidor <emanuella.altidor@gmail.com>
2021-04-19 11:49:46 -0400

    Framework Complete

---
+++ app.js
@@ -0,0 +1,52 @@
+// http://makeup-api.herokuapp.com/api/v1/products.json
+
+// Create dynamic drop down menu from first request
+
+
+
+
+
+
+
+// Create the form option menu/tags
+
+
+
+
+
+
+
+
+
+
+
+
+// Select tag value from drop down menu
+
+
+
+
+
+
+
+
+// Eventhandler for the Form
+
+
+
+
+
+
+
+
+
+// Create dynamic image tag and append to DOM
+
+
+
+
+
+
+
+
+// Remove previous product selection
