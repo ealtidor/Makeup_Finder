901eece4837c02487894c1fd870f72a8d050c032
ealtidor <emanuella.altidor@gmail.com>
2021-04-20 16:59:35 -0400

    Eye Dropdown Error Fixed

--- app.js
+++ app.js
@@ -7,15 +7,29 @@ const lipCat = document.querySelector('.lip-dropdown')
 const faceCat = document.querySelector('.face-dropdown')
 
 const getOptions = async () => {
-  // const url = 'http://makeup-api.herokuapp.com/api/v1/products.json'
   const url = "http://makeup-api.herokuapp.com/api/v1/products.json?product_type"
 
   try {
     const response = await axios.get(url)
-    console.log(response.data)
-    let eyeList = Object.keys(response.data)
-    // console.log(eyeList)
-    eyeOptions(eyeList)
+    const conArr = response.data.slice(0, 465)
+    console.log(conArr)
+    let eyeProd = []
+    let lipProd = []
+    let faceProd =[]
+    conArr.forEach((product) => {
+      if (product.product_type === "eyebrow" || product.product_type === "eyeliner" || product.product_type === "eyeshadow" || product.product_type === "mascara" ) {
+        eyeProd.push(product)
+      } else if (product.product_type === "lip_liner" || product.product_type === "lipstick" ) {
+        lipProd.push(product)
+      } else {
+        faceProd.push(product)
+      }
+})
+    console.log("eye", eyeProd)
+    console.log("lip", lipProd)
+    console.log("face",faceProd)
+  
+    eyeOptions(eyeProd)
 }
   catch (error) {
     console.error(error)
@@ -30,16 +44,22 @@ getOptions()
 
 
 function eyeOptions(list) {
-  // console.log(list)
-  const eyeCat = document.querySelector('.eye-dropdown')
-  list.forEach((product) => {
-    console.log(product)
+  
+  const eyeList = list.map((product) => {
+  return product.product_type
+  })
+  console.log(eyeList)
+  const uniqueEye = eyeList.filter((x, i, a) => a.indexOf(x) === i)
+  console.log(uniqueEye)
+  const eyeCat = document.querySelector('#select-eye')
+ uniqueEye.forEach((product) => {
     const eyeProduct = document.createElement('option')
-    eyeProduct.textContent = product
+    eyeProduct.innerText= product
     eyeProduct.value = product
     eyeCat.append(eyeProduct)
-  })
 
+  })
+  
 
 }
 
